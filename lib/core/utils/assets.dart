796f4f7583cf9f2c2dc9945942b75283d16efbdd
AhmedElesawy <elesawy928@gmail.com>
2025-01-27 14:37:55 +0200

    assets file

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesLogo = "assets/images/logo.png";
+
+  static const String imagesStart = "assets/images/start.png";
+
+  static const String svgImagesLogo = "assets/svgImages/logo.svg";
+}
+
