b92cd6e4bda349252346188e0676e7403d8c36d5
AhmedElesawy <elesawy928@gmail.com>
2025-03-06 03:52:31 +0200

    create owner search widget with logo

---
+++ lib/features/Owner/owner_home/presentation/views/widgets/owner_search_widget_with_logo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nile_brand/core/utils/sizes_padding.dart';
+
+import '../../../../../../core/utils/assets.dart';
+import '../../../../../../core/utils/styles.dart';
+import '../../../../../../core/widgets/app_text_form_field.dart';
+
+class OwnerSearchWidgetWithLogo extends StatelessWidget {
+  const OwnerSearchWidgetWithLogo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(right: 5.w, left: 5.w),
+          width: 275.w,
+          height: 36.h,
+          child: AppTextFormField(
+            contentPadding: EdgeInsets.symmetric(horizontal: 20.w),
+            cursorHeight: 22.h,
+            isLabled: false,
+            hintText: 'what are you looking for ?',
+            backgroundColor: Colors.white,
+            validator: (p0) {},
+            prefixIcon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+            raduis: 6.r,
+            hintStyle: Styles.font17W300,
+          ),
+        ),
+        12.hs,
+        Image.asset(
+          Assets.imagesProfileImage,
+          width: 36.r,
+          height: 36.r,
+        )
+      ],
+    );
+  }
+}
