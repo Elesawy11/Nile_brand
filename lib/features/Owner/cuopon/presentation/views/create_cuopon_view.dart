3427f04db43454e36b4e6f47aa4ace3b47faeb6a
AhmedElesawy <elesawy928@gmail.com>
2025-03-06 23:35:43 +0200

    create cuopon view

---
+++ lib/features/Owner/cuopon/presentation/views/create_cuopon_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nile_brand/core/utils/sizes_padding.dart';
+import 'package:nile_brand/core/utils/styles.dart';
+
+import '../../../../../core/utils/color_manager.dart';
+import '../../../../../core/widgets/app_text_button.dart';
+import '../../../../../core/widgets/app_text_form_field.dart';
+
+class CreateCuoponView extends StatelessWidget {
+  const CreateCuoponView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 22.w),
+          child: Column(
+            children: [
+              49.vs,
+              Text(
+                'Create Cuopon',
+                style: Styles.font35W700,
+              ),
+              33.vs,
+              SizedBox(
+                height: 60.h,
+                child: AppTextFormField(
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  hintText: "SUMMER2024",
+                  labelText: "Name",
+                  validator: (p0) {},
+                ),
+              ),
+              20.vs,
+              SizedBox(
+                height: 60.h,
+                child: AppTextFormField(
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  hintText: "2024, 8, 31",
+                  labelText: "expireTime",
+                  validator: (p0) {},
+                ),
+              ),
+              20.vs,
+              SizedBox(
+                height: 60.h,
+                child: AppTextFormField(
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  hintText: "20.0",
+                  labelText: "discount",
+                  validator: (p0) {},
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: 70.ph,
+                child: AppTextButton(
+                  backgroundColor: ColorManager.mainColor,
+                  text: "Save Changes",
+                  onPressed: () {
+                    context.pop();
+                  },
+                ),
+              ),
+              45.vs
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
