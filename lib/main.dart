11f28a2ea0d487438965a14637bb8bbd29baba1f
Norhan Helal <norhanhelal678@gmail.com>
2025-04-15 23:36:52 +0200

    firebase_setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 // import 'package:device_preview_plus/device_preview_plus.dart';
 // import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nile_brand/core/utils/service_locator.dart';
+import 'package:nile_brand/firebase_options.dart';
 import 'package:nile_brand/nile_brand.dart';
 import 'package:nile_brand/simple_bloc_observer.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   serviceLocator();
   Bloc.observer = SimpleBlocObserver();
   runApp(NileBrand());
-  // rubApp(
-  //  const NileBrand(),
-  // );
-  // runApp(
-  //   DevicePreview(
-  //     enabled: !kReleaseMode,
-  //     builder: (context) => const NileBrandBody(),
-  //   ),
-  // );
+  
 }
 
 class NileBrand extends StatelessWidget {
