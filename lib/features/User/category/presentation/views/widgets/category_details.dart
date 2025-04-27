402c63dc6db89efd4a025992ea8d6a6d498df0ca
Norhan Helal <norhanhelal678@gmail.com>
2025-04-27 18:13:27 +0300

    edit ui

--- lib/features/User/category/presentation/views/widgets/category_details.dart
+++ lib/features/User/category/presentation/views/widgets/category_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nile_brand/features/User/category/presentation/views/widgets/custome_item.dart';
 import 'package:nile_brand/features/User/category/presentation/views/widgets/subcategoriy_bar.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class CategoryDetails extends StatelessWidget {
   final String category;
@@ -16,17 +17,21 @@ class CategoryDetails extends StatelessWidget {
           SubcategoriyBar(category: category),
           Expanded(
             flex: 3,
-            child: GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 4.w,
-                  mainAxisSpacing: 4.w,
-                  childAspectRatio: 2 / 3),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return const CustomeItem();
-              },
+            // TODOS: shimmer effect
+            child:Skeletonizer(
+              enabled:false,
+              child: GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 4.w,
+                    mainAxisSpacing: 4.w,
+                    childAspectRatio: 2 / 3),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const CustomeItem();
+                },
+              ),
             ),
           ),
         ],
